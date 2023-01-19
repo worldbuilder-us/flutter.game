63afe44ccb9baf9690713e5869930779a5d9a940
Zyler <508282+zylerdj@users.noreply.github.com>
2023-01-19 09:58:14 +0100

    Play page with unique id for each play

--- pages/plays/[play_id].js
+++ pages/plays/[play_id].js
@@ -1,3 +1,4 @@
+
 import React, { useContext, useEffect, useState } from "react";
 import Head from "next/head";
 import { useTheme } from "next-themes";
@@ -6,16 +7,20 @@ import {
   PanelLayout,
   ThreeColumnLayout,
   WalletProviderPopUp,
-} from "../components";
-import { useRelay } from "../context/RelayContext";
-import TuningPanel from "../components/TuningPanel";
-import { useBitcoin } from "../context/BitcoinContext";
-import { useHaste } from '../hooks/useHaste'
+} from "../../components";
+import { useRelay } from "../../context/RelayContext";
+import TuningPanel from "../../components/TuningPanel";
+import { useBitcoin } from "../../context/BitcoinContext";
+import { useHaste } from '../../hooks/useHaste'
 import { useRouter } from 'next/router'
 
 import axios from 'axios'
 
-export default function Settings() {
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+
+export default function PlayGame() {
   const { theme, setTheme } = useTheme();
   const { logout: logoutHaste } = useHaste();
   const [isDark, setIsDark] = useState(theme === "dark");
@@ -40,7 +45,7 @@ export default function Settings() {
 
   const authenticated = tokenDetails?.isAuthenticated
 
-  async function playNow() {
+  async function donePlaying() {
     console.log('play now clicked')
 
     if (!authenticated) {
@@ -48,14 +53,16 @@ export default function Settings() {
       return
     }
 
-    const { data } = await axios.post(`https://fluttergame.fun/api/v1/haste/plays`, {
+    const { data } = await axios.post(`https://fluttergame.fun/api/v1/haste/scores`, {
       leaderboard_id: leaderboard,
-      handcash_token: tokenDetails.token
+      handcash_token: tokenDetails.token,
+      play: {
+        id: router.query.play_id
+      },
+      score: getRandomInt(1000)
     })
 
-    console.log('haste.play.created', data)
-
-    router.push(`/plays/${data.play.id}`)
+    console.log('haste.score.recorded', data)
 
   }
 
@@ -86,11 +93,12 @@ export default function Settings() {
     <PanelLayout>
       <div className="mx-auto max-w-xl col-span-12 lg:col-span-6 min-h-screen flex flex-col ">
         <div className="mt-7  p-4  ">
-        <img src={'flutter1-9-23_18.gif'} width='100%' layout='fill'/>
+        <img src={'/flutter1-9-23_18.gif'} width='100%' layout='fill'/>
+
 
           <div className="bg-gray-100 dark:bg-gray-600 p-5 flex flex-col cursor-pointer my-4 rounded-lg">
 
-            <TuningPanel closeAction={leaderboardSelected}/>
+            <p>Play ID: {router.query.play_id}</p>
           </div>
           {/* <div
             onClick={() => setWalletPopupOpen(true)}
@@ -122,10 +130,10 @@ export default function Settings() {
           </div> */}
 
           <button
-            onClick={playNow}
+            onClick={donePlaying}
             className="h-[52px] p-5 flex bg-red-500 text-white text-base font-semibold my-4 w-full border-none rounded-lg cursor-pointer items-center justify-center transition duration-500 transform hover:-translate-y-1 hover:bg-red-600"
           >
-            Play Now
+            Done Playing
           </button>
         </div>
         <div className="grow" />
